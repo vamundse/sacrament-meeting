46835df4da63b4e462f99c4e07a4cf34b1eeec31
vamundse <vamundse@gmail.com>
2026-09-14 13:43:29 +0200

    Sacrament meeting planner assignment

--- lib/api.ts
+++ lib/api.ts
@@ -3,5 +3,7 @@ export function getApiUrl(path: string): string {
     ? `https://${process.env.VERCEL_URL}`
     : 'http://localhost:3000';
   
-  return `${baseUrl}${path}`;
+  const url = `${baseUrl}${path}`;
+  console.log('[getApiUrl]', { VERCEL_URL: process.env.VERCEL_URL, baseUrl, path, url });
+  return url;
 }

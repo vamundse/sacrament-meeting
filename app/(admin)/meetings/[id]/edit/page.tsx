4e22f0a7cfcdd85011bad7930f9115df4ada44db
vamundse <vamundse@gmail.com>
2026-09-18 20:48:18 +0200

    added something to the edit page so it doesn't fail

---
+++ app/(admin)/meetings/[id]/edit/page.tsx
@@ -0,0 +1,9 @@
+export const dynamic = 'force-dynamic';
+
+export default function EditMeetingPage() {
+    return (
+        <div>
+            <h1>Edit Meeting - Is coming in week 04</h1>
+        </div>
+    );
+}
